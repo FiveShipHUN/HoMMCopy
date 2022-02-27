761a82a1f77b204dc92d30bf29f8bf90c98d42f7
Erik <erik200203@gmail.com>
2022-02-27 15:34:58 +0100

    Dev-phase

--- src/me/eriknikli/homm/scenes/GameScene.java
+++ src/me/eriknikli/homm/scenes/GameScene.java
@@ -5,14 +5,40 @@ import java.awt.*;
 
 public class GameScene extends Scene {
 
-    private GridBagLayout layout;
+    /**
+     * A layout maga
+     */
+    private final GridBagLayout layout;
+
+    /**
+     * A layout beállításait tartalmazza, ezen keresztül kontrollálhatjuk a komponenseket
+     */
+    private final GridBagConstraints constraints;
 
     /**
      * Inicializálja a scene-t
      */
     public GameScene() {
         layout = new GridBagLayout();
+        constraints = new GridBagConstraints();
         setLayout(layout);
+
+    }
+
+    /**
+     * Beállítások a komponensre
+     */
+    public GridBagConstraints actorSettings() {
+        return constraints;
+    }
+
+    /**
+     * Ezen keresztül lehet hozzáadni komponenseket a Scene-hez
+     *
+     * @param c komponens, amit szeretnénk hozzáadni a scene-hez
+     */
+    public void addActor(Component c) {
+        add(c, constraints);
     }
 
 
